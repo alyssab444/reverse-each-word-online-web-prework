3c864cad709b279c0e08f25aea4ba0466d59ade0
Alyssa Brown <alyssa.brown@live.com>
2019-01-05 03:32:45 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,10 @@
-def reverse_each_word(string)
-  string.each do |x|
-    x.reverse 
-  end 
-  
-end  
+def reverse_each_word_with_each(string)
+  original_array = string.split(" ")
+  return_array = []
+  original_array.each do|string|
+    return_array << string.reverse
+  end
+  return_array.join(" ")
+end
+
   
